1f927acf0092f23a27c3b54c523a0ad02423dc3f
Yossapol Witayanont <reef_3llen@hotmail.com>
2022-03-29 06:51:19 +0700

    add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE || 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: +process.env.DATABASE_PORT || 5432,
+      username: process.env.DATABASE_USER || 'dbuser',
+      password: process.env.DATABASE_PASSWORD || 'dbpassword',
+      database: process.env.DATABASE_NAME || 'app',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config({
+  path: '.env',
+});
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
